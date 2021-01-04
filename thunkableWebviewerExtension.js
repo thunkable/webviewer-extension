36ece18eaad43303ada7fb5639bb49a0d50c3adb
Paul M-W <paulmw@thunkable.com>
2021-01-04 09:33:08 -0800

    Add support for post message with return value

--- thunkableWebviewerExtension.js
+++ thunkableWebviewerExtension.js
@@ -1,18 +1,44 @@
-var ThunkableWebviewerExtension = {
-  postMessage: function (message) {
+var ThunkableWebviewerExtension = (function () {
+  const postMessageToWebview = (message) => {
     if (window.ReactNativeWebView) {
       window.ReactNativeWebView.postMessage(message);
     } else {
       window.parent.postMessage(message, '*');
     }
-  },
-  receiveMessage: function(fxn) {
-    var callbackFunction = function(event) {
-      if (typeof fxn === 'function') {
-        fxn(event.data)
+  };
+
+  const getReceiveMessageCallback = (fxn, hasReturnValue) => (event) => {
+    if (typeof fxn === 'function') {
+      if (event.data) {
+        let dataObject;
+        try {
+          dataObject = JSON.parse(event.data);
+        } catch (e) {
+          // message is not valid json
+        }
+        if (dataObject && dataObject.type === 'ThunkablePostMessage' && hasReturnValue) {
+          fxn(dataObject.message, (returnValue) => {
+            const returnMessageObject = { type: 'ThunkablePostMessageReturnValue', uuid: dataObject.uuid, returnValue };
+            postMessageToWebview(JSON.stringify(returnMessageObject));
+          });
+        } else if (!hasReturnValue && (!dataObject || dataObject.type !== 'ThunkablePostMessage')) {
+          fxn(event.data);
+        }
       }
-    };
-    document.addEventListener('message', callbackFunction, false);
-    window.addEventListener('message', callbackFunction, false);
-  }
-}
+    }
+  };
+
+  return {
+    postMessage: postMessageToWebview,
+    receiveMessage: function(fxn) {
+      const callbackFunction = getReceiveMessageCallback(fxn, false);
+      document.addEventListener('message', callbackFunction, false);
+      window.addEventListener('message', callbackFunction, false);
+    },
+    receiveMessageWithReturnValue: function(fxn) {
+      const callbackFunction = getReceiveMessageCallback(fxn, true);
+      document.addEventListener('message', callbackFunction, false);
+      window.addEventListener('message', callbackFunction, false);
+    },
+  };
+})();
